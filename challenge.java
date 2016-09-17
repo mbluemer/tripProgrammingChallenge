13c5f883b82ed4ec9955d7d5f9b16e63c1b79138
Mark Bluemer <bluemer.mark@gmail.com>
2016-09-17 16:15:15 -0400

    It's a naive solution

--- challenge.java
+++ challenge.java
@@ -4,8 +4,7 @@ class Main {
   public static void main (String[] args) {
     Scheduler test = new Scheduler();
     test.parseInput();
-    test.printJobs();
-    test.printWorkers();
+    test.run();
   }
 }
 
@@ -17,6 +16,25 @@ class Scheduler {
     JobComparator comp = new JobComparator();
     this._pq = new PriorityQueue<Job>(10, comp);
     this._workers = new ArrayList<Worker>();
+  }
+
+  public void run() {
+    int currentTime = 0;
+    while(!_pq.isEmpty()) {
+      Job j = _pq.poll();
+      //fast forward if necessary
+      if (j.canBeginTime > currentTime) currentTime = j.canBeginTime;
+
+      //Assign each worker to the current job at the same time
+      for (Worker w : _workers)
+        System.out.println(currentTime + " " + w.name + " " + j.name);
+
+      // Calculate time it takes to complete with all bots on
+      int tasksPerBot = (j.tasks % _workers.size() == 0) ? j.tasks / _workers.size() : j.tasks / _workers.size() + 1;
+      int timeToComplete = tasksPerBot * j.taskTime;
+
+      currentTime += timeToComplete;
+    }
   }
 
   public void parseInput() {
@@ -83,6 +101,7 @@ class Worker {
 class JobComparator implements Comparator<Job> {
   @Override
   public int compare(Job x, Job y) {
-    return y.priority - x.priority;
+    // We're going to make this the lowest start time for now
+    return x.canBeginTime - y.canBeginTime;
   }
 }
